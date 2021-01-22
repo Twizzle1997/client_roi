f609ad131697b985d5d7151e493684f576501d1f
Audrey <audrey.cornaire@gmail.com>
2021-01-22 10:58:22 +0100

    [UPDATE] last update à l'ajout d'un pays

--- config/db_access.py
+++ config/db_access.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from config.ressources import *
+from datetime import date
 import random
 
 class DatabaseManager:
@@ -58,20 +59,13 @@ class DatabaseManager:
         density = random.randint(10, 1000)
         land_area = random.randint(100000, 1000000)
         pop = random.randint(10000, 10000000)
-
-        # Density (P/Km²)	"119"
-        # Land Area (Km²)	"547557"
-        # Population (2020)	"65244628"
-        # _id	
-        # $oid	"60099b3652975e2bc055ed7d"
+        today = date.today()
 
         country={"Country": name, 
                     "Density (P/Km\u00b2)": density, 
                     "Land Area (Km\u00b2)": land_area, 
                     "Population (2020)": pop, 
-                    # "_id": {
-                    #     "$oid": "60099b3652975e2bc055ed7d"
-                    # }
+                    "Last Update" : str(today),
                     }
 
         self.__get_collection('countries_project', 'countries').insert_one(country)
